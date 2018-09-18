d77fa2c89ff74d0010a36e52435e63d4c4ecffd1
moqi <656017382@qq.com>
2018-09-18 08:42:25 +0800

    first commit

---
+++ ResultCls.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsAPI
+{
+    public class ResultCls
+    {
+        private int status;
+        public int Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+        private string message;
+        public string Message
+        {
+            get { return message; }
+            set { message = value; }
+        }
+    }
+}
---
+++ UserControl1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsAPI.USB;
+using Newtonsoft.Json;
+
+namespace WindowsAPI
+{
+    public partial class UserControl1: UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+            List<string> deviceList = new List<string>();
+            Hid.GetHidDeviceList(ref deviceList);
+            Console.WriteLine(deviceList);
+        }
+
+        /// <summary>
+        /// 获取设备hid
+        /// </summary>
+        /// <returns></returns>
+        public string GetHIDList() {
+            List<string> deviceList = new List<string>();
+            Hid.GetHidDeviceList(ref deviceList);
+            return JsonConvert.SerializeObject(deviceList);
+        }
+        
+    }
+}
